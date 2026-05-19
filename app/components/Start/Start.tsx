78fbfc0e3846acd0839c66cb9bf54989a98897f3
Lythmass <lythmass17@gmail.com>
2026-05-19 19:40:25 +0400

    style: add animatinos

--- app/components/Start/Start.tsx
+++ app/components/Start/Start.tsx
@@ -9,7 +9,7 @@ export const Start = ({ setHasStartedSession }: StartProps) => {
   return (
     <div className="w-full relative h-full flex items-center justify-center flex-col">
       <div className="flex absolute top-5 flex-col gap-3 items-center">
-        <p className={`text-5xl text-gray-900 ${dmsans.className} font-bold `}>
+        <p className={`text-5xl text-gray-800 ${dmsans.className} font-bold `}>
           Call Convo Boy
         </p>
         <p className={`text-sm text-gray-600 ${nunito.className} `}>
@@ -19,7 +19,7 @@ export const Start = ({ setHasStartedSession }: StartProps) => {
       <div className="flex mt-25 flex-col items-center gap-2">
         <div
           onClick={() => setHasStartedSession(true)}
-          className="flex items-center gap-2 shadow-lg hover:shadow-2xl hover:scale-[1.05] transition-all cursor-pointer px-4 py-3 rounded-full bg-linear-to-r from-[#00C9B5] to-[#14C900]"
+          className="flex items-center bg-size-[200%_200%] gap-2 shadow-lg hover:shadow-2xl hover:scale-[1.05] transition-all cursor-pointer px-4 py-3 rounded-full bg-linear-to-r from-[#00C9B5] to-[#14C900] hover:animate-gradient-flow"
         >
           <CallSVG className="mr-1" width={30} height={30} fill="white" />
           <p className={`font-bold text-white ${nunito.className} `}>
--- app/page.tsx
+++ app/page.tsx
@@ -19,9 +19,9 @@ export default function ChatPage() {
     <>
       {hasStartedSession && <Background />}
 
-      <div className="w-full bg-linear-120 from-[#102428] to-[#141028] h-screen flex justify-center items-center mx-auto p-8">
+      <div className="w-full bg-size-[400%_400%] animate-gradient-flow bg-linear-120 from-[#102428] to-[#141028] h-screen flex justify-center items-center mx-auto p-8">
         <div
-          className={`py-7 flex justify-center items-start shadow-2xl rounded-4xl shadow-2xl bg-linear-to-r from-[#C5E8D8] to-[#C5D5E8]  ${!hasStartedSession && !hasFinishedSession ? "h-80 w-120" : "h-[calc(100vh-5rem)] max-w-xl w-full"}`}
+          className={`py-7 flex justify-center items-start shadow-2xl rounded-4xl bg-linear-to-r from-[#C5E8D8] to-[#C5D5E8]  ${!hasStartedSession && !hasFinishedSession ? "h-80 w-120" : "h-[calc(100vh-5rem)] max-w-xl w-full"}`}
         >
           {hasStartedSession && (
             <Speak
